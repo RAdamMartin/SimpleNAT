72047bd949b73e87a695bf1a1ce9b2155893645a
Adam Martin <adam.martin223@gmail.com>
2015-11-27 17:56:05 -0500

    checking port unreachable

--- new_router/sr_arpcache.c
+++ new_router/sr_arpcache.c
@@ -141,7 +141,7 @@ struct sr_arpreq *sr_arpcache_insert(struct sr_arpcache *cache,
         cache->entries[i].added = time(NULL);
         cache->entries[i].valid = 1;
     }
-    sr_arpcache_dump(cache);
+    /*sr_arpcache_dump(cache);*/
     pthread_mutex_unlock(&(cache->lock));
     
     return req;
