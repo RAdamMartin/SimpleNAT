2b19418ef50759aa4e5bc81c01aa46c2dca3e017
Adam Martin <adam.martin223@gmail.com>
2015-12-04 16:05:21 -0500

    Added tcp print

--- new_router/sr_router.c
+++ new_router/sr_router.c
@@ -190,6 +190,18 @@ void natHandleIPPacket(struct sr_instance* sr,
             sr_send_icmp(sr, packet, len, 11, 0,0);
         } else if(ip_header->ip_p==6) { /*TCP*/
             fprintf(stderr,"FWD TCP from int\n");
+            sr_tcp_hdr_t * tcp_header = (sr_tcp_hdr_t*)(packet+SIZE_ETH+SIZE_IP);
+            fprintf(stderr, "\toff:\t%u",tcp_header->tcp_off);
+            fprintf(stderr, "\tns:\t%u", tcp_header->tcp_ns);
+            fprintf(stderr, "\tres:\t%u",tcp_header->reserved);
+            fprintf(stderr, "\tcwr:\t%u",tcp_header->tcp_cwr);
+            fprintf(stderr, "\tece:\t%u",tcp_header->tcp_ece);
+            fprintf(stderr, "\turg:\t%u",tcp_header->urg);
+            fprintf(stderr, "\tack:\t%u",tcp_header->ack);
+            fprintf(stderr, "\tpsh:\t%u",tcp_header->psh);
+            fprintf(stderr, "\trst:\t%u",tcp_header->rst);
+            fprintf(stderr, "\tsyn:\t%u",tcp_header->syn);
+            fprintf(stderr, "\tfin:\t%u",tcp_header->fin);
         } else if(ip_header->ip_p==1 ) { /*ICMP*/
             fprintf(stderr,"FWD ICMP from int\n");
             sr_icmp_t8_hdr_t * icmp_header = (sr_icmp_t8_hdr_t*)(packet+SIZE_ETH+SIZE_IP);
